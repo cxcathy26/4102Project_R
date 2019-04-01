b2a5270750bbe2765c84fdce06e4a04058d0c2a4
caj14011 <cathryn.jenner@uconn.edu>
2019-04-01 17:40:59 -0400

    word freq is done? need to check it

--- wordfreq.R
+++ wordfreq.R
@@ -1,8 +1,6 @@
 fileName <- 'input.txt'
 outFile <- 'output.txt'
 
-x <- readChar(fileName, file.info(fileName)$size)
-
 con = file(fileName, "r")
 
 sentences <-readLines(con)
@@ -10,12 +8,30 @@ scan("input.txt","character",sep="\n")
 
 sentences<-gsub("([_-])|[[:punct:]]","\\1",sentences)
 sentences<-gsub("\\.","",sentences)
+print(sentences)
+sentences <- gsub("^[[:digit:]]+" , "", sentences) #removes digits in front of word
+print(sentences)
+sentences <- gsub("^-+" , "",sentences)
+sentences <- gsub("^_+" , "",sentences) #is regular expression
+#sentences[!sapply(sentences, identical, character(0))]
+newsen <- unlist(strsplit(sentences, "\ "))
 
+print(newsen)
+newsen <- gsub("^[[:digit:]]*" , "", newsen) #removes digits in front of word
+newsen <- gsub("^-+" , " ", newsen)
+newsen <- gsub("^_+" , " ", newsen) #is regular expression
+#print(newsen)
+#newsen <- gsub("[[:blank:]]*" , " ", newsen)
+#sentences <- paste(newsen, sep= " ")
+
+#-----------------------------------------------------------
+#-----------------------------------------------------------
 #NEED TO DISCARD - _ OR NUM IF THE WORD STARTS WITH THEM 
 #probably using SUB() , subs for one occurence
 #use it within an if statement
 
 #Split sentence
-word_counts <- as.data.frame(table(unlist( strsplit(sentences, "\ ") )))  # split vector by space
+word_counts <- as.data.frame(table(unlist( strsplit(newsen, "\ ") )))  # split vector by space
 
 print(word_counts)
+close(con)
